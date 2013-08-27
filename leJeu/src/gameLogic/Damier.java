0f351e5477be6fbdabce49226dcce5204a8d71e5
tonton phil <tonton.phil@gmail.com>
2013-08-27 23:34:27 +0200

    1ers fichiers

---
+++ leJeu/src/gameLogic/Damier.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package gameLogic;
+
+/**
+ * @author Philippe
+ *
+ */
+public class Damier {
+	
+	int largeur, longueur;
+
+	public Damier(int largeur, int longueur) {
+		this.largeur = largeur;
+		this.longueur = longueur;
+	}
+	
+	public Damier() {
+		Damier(8,8);
+	}
+	
+}
---
+++ leJeu/src/gameLogic/jeton.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package gameLogic;
+
+/**
+ * @author Philippe
+ *
+ */
+public class jeton {
+
+	int poid;
+	int taille;
+	
+}
