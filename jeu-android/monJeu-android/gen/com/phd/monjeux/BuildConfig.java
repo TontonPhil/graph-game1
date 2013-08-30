c148b0165ca04f6a68885be69d45b1e160da439b
tonton phil <tonton.phil@gmail.com>
2013-08-30 22:43:03 +0200

    version initiale auto-générée

---
+++ jeu-android/monJeu-android/gen/com/phd/monjeux/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.phd.monjeux;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- leJeu/src/gameLogic/jeton.java
+++ leJeu/src/gameLogic/jeton.java
@@ -9,7 +9,17 @@ package gameLogic;
  */
 public class jeton {
 
-	int poid;
+	int poids;
 	int taille;
+	
+	public jeton() {
+		this.poids=1;
+		this.taille=1;
+	}
+	
+	// TODO verifier que c'est bien un void, et pas le dessin lui-même
+	public void explose (int frameNb);
+	
+	public void tombe (int frameNb);
 	
 }
